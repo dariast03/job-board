24b38c130584e1c2b0abcc37342fcd27b6f6ad99
Dante Arias <danteariastarifa@gmail.com>
2024-03-03 15:58:47 -0400

    Add layout component to routes.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,33 +2,45 @@ import { createBrowserRouter } from 'react-router-dom'
 import GeneralError from '../pages/errors/general-error'
 import NotFoundError from '../pages/errors/not-found-error'
 import MaintenanceError from '../pages/errors/maintenance-error'
+import Layout from '../components/layout/main-layout'
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        lazy: async () => {
+        /*        lazy: async () => {
             const AppShell = await import('../components/layout/main-layout')
             return { Component: AppShell.default }
-        },
+        }, */
         errorElement: import.meta.env.PROD && <GeneralError />,
         children: [
             {
-                index: true,
-                lazy: async () => ({
-                    Component: (await import('../pages/index')).default,
-                }),
+                element: <Layout withHero />,
+                children: [
+                    {
+                        path: '',
+                        lazy: async () => ({
+                            Component: (await import('../pages/index')).default,
+                        }),
+                    },
+                ],
             },
             {
-                path: 'ofertas-de-trabajo',
-                lazy: async () => ({
-                    Component: (await import('../pages/jobs')).default,
-                }),
-            },
-            {
-                path: 'ofertas-de-trabajo/:id',
-                lazy: async () => ({
-                    Component: (await import('../pages/jobs/[id]')).default,
-                }),
+                element: <Layout />,
+                children: [
+                    {
+                        path: 'ofertas-de-trabajo',
+                        lazy: async () => ({
+                            Component: (await import('../pages/jobs')).default,
+                        }),
+                    },
+                    {
+                        path: 'ofertas-de-trabajo/:id',
+                        lazy: async () => ({
+                            Component: (await import('../pages/jobs/[id]'))
+                                .default,
+                        }),
+                    },
+                ],
             },
         ],
     },
