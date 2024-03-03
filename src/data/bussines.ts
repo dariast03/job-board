2fb19d1ec7ed5c1be64478c507a8bbadd1279bf0
AlexanderRG <alexander.rgareca@gmail.com>
2024-03-03 14:17:52 -0400

    Agregados registros de negocios

--- src/data/bussines.ts
+++ src/data/bussines.ts
@@ -19,4 +19,24 @@ export const bussines = [
         nombre: 'Apple',
         logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Apple_Computer_Logo_rainbow.svg/858px-Apple_Computer_Logo_rainbow.svg.png',
     },
+    {
+        id: 5,
+        nombre: 'Amazon',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg',
+    },
+    {
+        id: 6,
+        nombre: 'Samsung',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/1200px-Samsung_Logo.svg.png?20221128191222',
+    },
+    {
+        id: 7,
+        nombre: 'Xiaomi',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Xiaomi_logo_%282021-%29.svg/768px-Xiaomi_logo_%282021-%29.svg.png?20221112002517',
+    },
+    {
+        id: 8,
+        nombre: 'Sony',
+        logo: 'https://logos-world.net/wp-content/uploads/2020/04/Sony-Logo-700x394.png',
+    },
 ]
